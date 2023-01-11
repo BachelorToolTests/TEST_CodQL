8db92563342e5f6b046b21b7a88b3b3b52815dde
PRSoftRafael <bakaschevr@gmail.com>
2023-01-11 18:53:09 +0100

    more test-methods

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -6,7 +6,7 @@ public class Main {
 
     }
 
-    private static void test1() throws IOException {
+    private void test1() throws IOException {
         BufferedWriter bw = new BufferedWriter(new FileWriter("test.txt"));
         bw.write("Hello World");
     }
@@ -39,6 +39,56 @@ public class Main {
     }
 
     private void test5() throws IOException {
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new FileWriter("test.txt"));
+            bw.write("Hello World");
+        }
+        catch (Exception e) { }
+        finally {
+            try{
+                if(bw != null) {
+                    bw.close();
+                }
+            } catch(Exception e){}
+        }
+    }
+
+    //===========================================================================
+
+    private void test6() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter("test.txt"));
+        bw.write("Hello World");
+    }
+
+    private void test7() throws IOException {
+        BufferedWriter bw = new BufferedWriter(new FileWriter("test.txt"));
+        bw.write("Hello World");
+        bw.close();
+    }
+
+    private void test8() throws IOException {
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new FileWriter("test.txt"));
+            bw.write("Hello World");
+        }
+        catch (Exception e) { }
+    }
+
+    private void test9() throws IOException {
+        BufferedWriter bw = null;
+        try {
+            bw = new BufferedWriter(new FileWriter("test.txt"));
+            bw.write("Hello World");
+        }
+        catch (Exception e) { }
+        finally {
+            bw.close();
+        }
+    }
+
+    private void test10() throws IOException {
         BufferedWriter bw = null;
         try {
             bw = new BufferedWriter(new FileWriter("test.txt"));
