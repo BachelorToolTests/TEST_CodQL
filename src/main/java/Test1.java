e8499a69879c2f9940b5bf50652403a4783c02ea
PRSoftRafael <bakaschevr@gmail.com>
2023-01-16 10:31:37 +0100

    some more tests

--- src/main/java/Test1.java
+++ src/main/java/Test1.java
@@ -20,4 +20,174 @@ public class Test1 {
             fileStream.close();
         }
     }
+
+    protected void test12() {
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("test"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void test22(File file) throws IOException {
+        FileInputStream fileStream = null;
+        try {
+            fileStream = new FileInputStream(file);
+            int i = fileStream.read();
+        }
+        catch (Exception e) {
+            fileStream.close();
+        }
+    }
+
+    protected void test13() {
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("test"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void test23(File file) throws IOException {
+        FileInputStream fileStream = null;
+        try {
+            fileStream = new FileInputStream(file);
+            int i = fileStream.read();
+        }
+        catch (Exception e) {
+            fileStream.close();
+        }
+    }
+
+    protected void test14() {
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("test"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void test24(File file) throws IOException {
+        FileInputStream fileStream = null;
+        try {
+            fileStream = new FileInputStream(file);
+            int i = fileStream.read();
+        }
+        catch (Exception e) {
+            fileStream.close();
+        }
+    }
+
+    protected void test15() {
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("test"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void test25(File file) throws IOException {
+        FileInputStream fileStream = null;
+        try {
+            fileStream = new FileInputStream(file);
+            int i = fileStream.read();
+        }
+        catch (Exception e) {
+            fileStream.close();
+        }
+    }
+
+    protected void test16() {
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("test"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void test26(File file) throws IOException {
+        FileInputStream fileStream = null;
+        try {
+            fileStream = new FileInputStream(file);
+            int i = fileStream.read();
+        }
+        catch (Exception e) {
+            fileStream.close();
+        }
+    }
+
+    protected void test17() {
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("test"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void test27(File file) throws IOException {
+        FileInputStream fileStream = null;
+        try {
+            fileStream = new FileInputStream(file);
+            int i = fileStream.read();
+        }
+        catch (Exception e) {
+            fileStream.close();
+        }
+    }
+
+    protected void test18() {
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("test"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void test28(File file) throws IOException {
+        FileInputStream fileStream = null;
+        try {
+            fileStream = new FileInputStream(file);
+            int i = fileStream.read();
+        }
+        catch (Exception e) {
+            fileStream.close();
+        }
+    }
+
+    protected void test19() {
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("test"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void test29(File file) throws IOException {
+        FileInputStream fileStream = null;
+        try {
+            fileStream = new FileInputStream(file);
+            int i = fileStream.read();
+        }
+        catch (Exception e) {
+            fileStream.close();
+        }
+    }
+    protected void test10() {
+        try{
+            BufferedReader br = new BufferedReader(new FileReader("test"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+
+    protected void test20(File file) throws IOException {
+        FileInputStream fileStream = null;
+        try {
+            fileStream = new FileInputStream(file);
+            int i = fileStream.read();
+        }
+        catch (Exception e) {
+            fileStream.close();
+        }
+    }
 }
